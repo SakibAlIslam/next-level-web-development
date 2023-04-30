b796fc2e61cb3c67b224d7c130a2b3a286180b64
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-04-30 22:52:56 +0600

    primitive type learning

--- module-1/src/index.ts
+++ module-1/src/index.ts
@@ -1,2 +1,29 @@
-const course: string = 'Typescript new course';
-console.log(course);
+//Primitive types
+
+//any...............
+let myBatch; //type any (don't use it)
+let myFirstDeclaration: string; //type annotation (recommended)
+myFirstDeclaration = 'Hello from typescript'; //type annotation
+
+//string....................
+let courseName: string = 'Typescript new course'; //type annotation
+let teamName = 'A team'; //type inference
+courseName = 'Typescript new course: next level development'; 
+
+//number....................
+const rollNumber: number = 123456; //type annotation
+const SeatNumber = 123456; //type inference
+
+//boolean....................
+const isCompleted: boolean = false;     //type annotation
+const isAdmin = false;                 //type inference
+
+//null....................
+const nullValue: null = null; //type annotation
+const nullValue2 = null; //type inference
+
+//undefined....................
+const undefinedValue: undefined = undefined;
+
+//symbol....................
+const symbolValue: symbol = Symbol('symbol');
