224a1447a66af2f931195f451426a6ed947c73f3
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-05-02 03:10:55 +0600

    enum and union and intersection learning

---
+++ module-1/src/union-intersaction.ts
@@ -0,0 +1,39 @@
+//enum
+enum Level {
+    mid= "mid",
+    senior= "senior",
+    lead= "lead",
+}
+
+type NoobDeveloper = {
+    name: string;
+}
+
+type JuniorDeveloper = {
+    name: string;
+    expertise: string;
+    experience: number;
+}
+
+type NextLevelDeveloper = JuniorDeveloper & {
+    leaderShip: string;
+    level: Level
+}
+
+//Union type
+const newDeveloper: NoobDeveloper | JuniorDeveloper = {
+    name: 'Sakib',
+    expertise: 'React',
+    experience: 2
+}
+
+//Intersection type
+const midLevelDeveloper: NextLevelDeveloper = {
+    name: 'Sakib',
+    expertise: 'React, Typescript',
+    experience: 2,
+    leaderShip: 'Team Lead',
+    level: Level.mid
+}
+
+console.log('midLevelDeveloper: ', midLevelDeveloper);
