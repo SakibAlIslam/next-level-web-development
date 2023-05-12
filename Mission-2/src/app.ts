6589fb90f116ee706ebf118a5da3fb08c64fb9fd
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-05-12 21:34:39 +0600

    app.ts is matured

--- Mission-2/src/app.ts
+++ Mission-2/src/app.ts
@@ -2,3 +2,11 @@ import express from 'express';
 import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
+
+app.use('/', (req, res) => {
+    res.send('Running in browser');
+});
+
+export default app;
--- Mission-2/src/server.ts
+++ Mission-2/src/server.ts
@@ -1,24 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import app from './app';
+
 const port = 5000;
-const app = express();
 
 //database connection..
 const bootstrap = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/practiceMongoose");
     console.log("database connected succesfully");
+    app.listen(port, () => {
+      console.log(`My server listening at Port: ${port}`);
+    });
   } catch (error) {
     console.log("Falied to connect: ", error);
   }
-}
+};
 
 bootstrap();
-
-app.use("/", (req, res) => {
-  res.send("hello");
-});
-
-app.listen(port, () => {
-  console.log(`My server listening at Port: ${port}`);
-});
