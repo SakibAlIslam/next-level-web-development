d8eb7400877553459de6640ae1176c209d0b8c69
SakibAlIslam <sakib35-1680@diu.edu.bd>
2023-05-12 21:58:07 +0600

    schema running process

--- Mission-2/src/app.ts
+++ Mission-2/src/app.ts
@@ -1,11 +1,50 @@
-import express from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors'
+import { Schema, model } from 'mongoose';
 
-const app = express();
+const app: Application = express();
 
 app.use(cors());
 
-app.use('/', (req, res) => {
+//parse data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    //inserting a test data in mongodb
+
+    /*
+    step1: Interface
+    step2: Schema
+    step3: Model
+    step4: Query
+    */
+
+    //creating an interface
+    interface IUser {
+        name: {
+            firstName: string;
+            middleName: string;
+            lastName: string
+        };
+        age: number;
+        id: string;
+        role: 'student';
+        password: string;
+        dateOfBirth?: string;
+        gender: 'male' | 'female';
+        email?: string;
+        contactNo: string;
+        emergencyConNo: string;
+        presentAddress: string;
+        permanentAddress: string;
+    }
+
+    //creating schema using Interface:
+    const userSchema = new Schema<IUser>({
+
+    })
+
     res.send('Running in browser');
 });
 
